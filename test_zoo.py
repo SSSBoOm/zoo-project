b92fea6597f88f61b15710b4ec0484f2f8339788
SSSBoOm <boom1909chanapat@gmail.com>
2024-11-18 17:00:51 +0700

    feat: Refactor ticket pricing logic and expand test coverage

--- test_zoo.py
+++ test_zoo.py
@@ -1,14 +1,32 @@
 import unittest
 from zoo import Zoo
 
+
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_baby_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
 
-if __name__ == '__main__':
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(30), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+
+    def test_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(200), 100)
+
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+
+
+if __name__ == "__main__":
     unittest.main()
